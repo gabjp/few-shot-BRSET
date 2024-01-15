c48cffe9d149f7f1f15ac2cb1892474f6e4d30f7
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2024-01-15 20:45:45 -0300

    update

--- utils.py
+++ utils.py
@@ -74,6 +74,82 @@ class SmallNet(nn.Module):
 
 		return torch.flatten(self.m(x))
 	
+class VGG16(nn.Module):
+	def __init__(self, num_classes = 3, dropout = 0.3):
+		super().__init__()
+
+		self.features = nn.Sequential(
+			nn.Conv2d(12, 64, kernel_size=3, stride=1, padding=1),
+			
+			nn.ReLU(),
+			nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+			
+			nn.ReLU(),
+			nn.MaxPool2d(kernel_size=2, stride = 2),
+			nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+			
+			nn.ReLU(),
+			nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+			
+			nn.ReLU(),
+			nn.MaxPool2d(kernel_size=2, stride = 2),
+			nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+			
+			nn.ReLU(),
+			nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+			
+			nn.ReLU(),
+			nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+			
+			nn.ReLU(),
+			nn.MaxPool2d(kernel_size=2, stride = 2),
+			nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+			
+			nn.ReLU(),
+			nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+			
+			nn.ReLU(),
+			nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+			
+			nn.ReLU(),
+			nn.MaxPool2d(kernel_size=2, stride = 2),
+			nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+			
+			nn.ReLU(),
+			nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+			
+			nn.ReLU(),
+			nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+			
+			nn.ReLU(),
+			nn.MaxPool2d(kernel_size=2, stride = 2))
+
+		# Camadas Fully Connected com menos parâmetros que a implementação original:
+
+		self.fc = nn.Sequential(
+			nn.Dropout(dropout),
+			nn.Linear(6144, 1024),
+			nn.ReLU())
+
+		self.fc1 = nn.Sequential(
+			nn.Dropout(dropout),
+			nn.Linear(1024, 512),
+			nn.ReLU())
+
+		self.fc2 = nn.Sequential(
+			nn.Linear(512, 1))
+		
+		self.m = nn.Sigmoid()
+		return
+
+	def forward(self, x):
+		out = self.features(x)
+		out = torch.reshape(out, (-1, 512))
+		out = self.fc(out)
+		out = self.fc1(out)
+		out = self.fc2(out)
+		return torch.flatten(self.m(out))
+	
 def test(testloader, model, criterion, device):
 	model.eval()
 	epoch_loss = 0
