0f19330e0f46ce6e0c6a8fac144adb76f2405478
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2024-01-16 01:58:54 -0300

    update

--- utils.py
+++ utils.py
@@ -17,7 +17,7 @@ class DumbNet(nn.Module):
 
 			nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
 			nn.LeakyReLU(),
-			nn.MaxPool2d(kernel_size=2, stride = 2),
+			nn.MaxPool2d(kernel_size=4, stride = 4),
 
 			nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
 			nn.LeakyReLU(),
