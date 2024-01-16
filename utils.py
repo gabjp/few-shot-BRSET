ee5a4d4ecea16be52aed7fac6c2ffed1bc37d840
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2024-01-15 23:59:37 -0300

    update

--- utils.py
+++ utils.py
@@ -5,26 +5,57 @@ import matplotlib.pyplot as plt
 class DumbNet(nn.Module):
 	def __init__(self):
 		super(DumbNet, self).__init__()
+
+		self.features = nn.Sequential(
+			nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+			
+			nn.LeakyReLU(),
+			nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+			
+			nn.LeakyReLU(),
+			nn.MaxPool2d(kernel_size=2, stride = 2),
+			nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+			
+			nn.LeakyReLU(),
+			nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+			
+			nn.LeakyReLU(),
+			nn.MaxPool2d(kernel_size=2, stride = 2),
+			nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+			
+			nn.LeakyReLU(),
+			nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+			
+			nn.LeakyReLU(),
+			nn.MaxPool2d(kernel_size=2, stride = 2),
+			nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+			
+			nn.LeakyReLU(),
+			nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+			
+			nn.LeakyReLU(),
+			nn.MaxPool2d(kernel_size=2, stride = 2))
+
 		self.fc = nn.Sequential(
-			nn.Linear(3 * 1024, 1024),
+			nn.Linear(4096, 2048),
 			nn.LeakyReLU())
 
 		self.fc1= nn.Sequential(
-			nn.Linear(1024, 512),
+			nn.Linear(2048, 1024),
 			nn.LeakyReLU())
 
 		self.fc2 = nn.Sequential(
-			nn.Linear(512, 256),
+			nn.Linear(1024, 512),
 			nn.LeakyReLU())
 
 		self.fc3 = nn.Sequential(
-			nn.Linear(256, 1))
+			nn.Linear(512, 1))
 		
 		self.m = nn.Sigmoid()
 	
-	def forward(self,x):
-
-		out = torch.flatten(x, start_dim=1)
+	def forward(self,out):
+		out = self.features(out)
+		out = torch.flatten(out, start_dim=1)
 		out = self.fc(out)
 		out = self.fc1(out)
 		out = self.fc2(out)
