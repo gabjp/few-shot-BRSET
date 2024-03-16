6f8e5d465c88a7969839e49942549aab97541598
Gabriel Jacob Perin (IC | nina) <gabrieljp@usp.br>
2024-03-15 23:48:36 -0300

    update

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from timm.models import vision_transformer as model_family
 
 # Define the base model variant to use
 base_model = 'vit_small_patch32_224'
-version = "augreg_in21k_ft_in1k"
+version = "augreg_in1k"
 
 # Get the default configuration of the chosen model
 model_cfg = model_family.default_cfgs[base_model].default.to_dict()
@@ -16,4 +16,4 @@ print(model_cfg)
 
 # swin_s3_tiny_224.ms_in1k 'mean': (0.485, 0.456, 0.406), 'std': (0.229, 0.224, 0.225)
 
-# vit_small_patch32_224.augreg_in21k_ft_in1k 'mean': (0.5, 0.5, 0.5), 'std': (0.5, 0.5, 0.5)
+# vit_base_patch32_224.augreg_in1k 'mean': (0.5, 0.5, 0.5), 'std': (0.5, 0.5, 0.5)
